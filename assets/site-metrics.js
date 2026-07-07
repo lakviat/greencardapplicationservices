bfb415cdc56d21021179c2a5d0a3f2e2f2c9262a
Nurlan Mirovich <lakviat@yahoo.com>
2026-07-07 16:04:50 -0500

    Add Google Analytics tag

---
+++ assets/site-metrics.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+
+window.gtag = function gtag() {
+  window.dataLayer.push(arguments);
+};
+
+window.gtag("js", new Date());
+window.gtag("config", "G-9P726CYPGF");
